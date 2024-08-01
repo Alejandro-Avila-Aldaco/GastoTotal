e1fbc3fded54f901e0c9730b76061a8a22218e9a
Nargul <74679147+Alejandro-Avila-Aldaco@users.noreply.github.com>
2024-07-31 20:41:32 -0600

    Add files via upload

---
+++ src/main/webapp/prueba.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+}
